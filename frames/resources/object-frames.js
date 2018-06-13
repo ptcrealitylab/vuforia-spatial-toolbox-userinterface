6c9317421c62ae8c1bfb6cceec4c1df6dffe8523
Ben Reynolds <breynolds@ptc.com>
2018-06-13 15:24:15 -0400

    only updates public data on frame becomes visible, not hidden

--- frames/resources/object-frames.js
+++ frames/resources/object-frames.js
@@ -161,13 +161,6 @@ var sendTouchEvents = false;
         if (typeof msgContent.visibility !== 'undefined') {
             realityObject.visibility = msgContent.visibility;
             
-            // reload public data
-            for (var i = 0; i < realityInterfaces.length; i++) {
-                if (typeof realityInterfaces[i].ioObject.emit !== 'undefined') {
-                    realityInterfaces[i].ioObject.emit('/subscribe/realityEditor', JSON.stringify({object: realityObject.object, frame: realityObject.frame}));
-                }
-            }
-            
             if(realityObject.visibility === "visible"){
                 if (typeof realityObject.node !== "undefined") {
                     if(realityObject.sendSticky) {
@@ -184,6 +177,13 @@ var sendTouchEvents = false;
                                 fullScreen: realityObject.sendFullScreen,
                                 stickiness: realityObject.sendSticky
                             }), "*");
+                    }
+                }
+
+                // reload public data when it becomes visible
+                for (var i = 0; i < realityInterfaces.length; i++) {
+                    if (typeof realityInterfaces[i].ioObject.emit !== 'undefined') {
+                        realityInterfaces[i].ioObject.emit('/subscribe/realityEditor', JSON.stringify({object: realityObject.object, frame: realityObject.frame}));
                     }
                 }
             }
