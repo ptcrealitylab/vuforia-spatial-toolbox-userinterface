fc0e6c9aea6a527609af6164162d2350c3d01010
Ben Reynolds <breynolds@ptc.com>
2018-07-17 11:27:16 -0400

    creates app/callbacks.js to provide a central location for all API calls triggered from native iOS code

---
+++ src/app/callbacks.js
@@ -0,0 +1,51 @@
+/**
+ * @preserve
+ *
+ *                                      .,,,;;,'''..
+ *                                  .'','...     ..',,,.
+ *                                .,,,,,,',,',;;:;,.  .,l,
+ *                               .,',.     ...     ,;,   :l.
+ *                              ':;.    .'.:do;;.    .c   ol;'.
+ *       ';;'                   ;.;    ', .dkl';,    .c   :; .'.',::,,'''.
+ *      ',,;;;,.                ; .,'     .'''.    .'.   .d;''.''''.
+ *     .oxddl;::,,.             ',  .'''.   .... .'.   ,:;..
+ *      .'cOX0OOkdoc.            .,'.   .. .....     'lc.
+ *     .:;,,::co0XOko'              ....''..'.'''''''.
+ *     .dxk0KKdc:cdOXKl............. .. ..,c....
+ *      .',lxOOxl:'':xkl,',......'....    ,'.
+ *           .';:oo:...                        .
+ *                .cd,      ╔═╗┌┬┐┬┌┬┐┌─┐┬─┐    .
+ *                  .l;     ║╣  │││ │ │ │├┬┘    '
+ *                    'l.   ╚═╝─┴┘┴ ┴ └─┘┴└─   '.
+ *                     .o.                   ...
+ *                      .''''','.;:''.........
+ *                           .'  .l
+ *                          .:.   l'
+ *                         .:.    .l.
+ *                        .x:      :k;,.
+ *                        cxlc;    cdc,,;;.
+ *                       'l :..   .c  ,
+ *                       o.
+ *                      .,
+ *
+ *      ╦═╗┌─┐┌─┐┬  ┬┌┬┐┬ ┬  ╔═╗┌┬┐┬┌┬┐┌─┐┬─┐  ╔═╗┬─┐┌─┐ ┬┌─┐┌─┐┌┬┐
+ *      ╠╦╝├┤ ├─┤│  │ │ └┬┘  ║╣  │││ │ │ │├┬┘  ╠═╝├┬┘│ │ │├┤ │   │
+ *      ╩╚═└─┘┴ ┴┴─┘┴ ┴  ┴   ╚═╝─┴┘┴ ┴ └─┘┴└─  ╩  ┴└─└─┘└┘└─┘└─┘ ┴
+ *
+ *
+ * Created by Ben Reynolds on 7/17/18.
+ */
+
+createNameSpace("realityEditor.app.callbacks");
+
+/**
+ * @fileOverview realityEditor.app.callbacks.js
+ * The central location where all functions triggered from within the native iOS code should reside.
+ * These can just be simple routing functions that trigger the appropriate function in other files,
+ * but this acts to organize all API calls in a single place.
+ */
+
+// response with a callback that indicates the device name.
+realityEditor.app.callbacks.addHeartbeatObject = function(beat) {
+    realityEditor.network.addHeartbeatObject(beat);
+};
