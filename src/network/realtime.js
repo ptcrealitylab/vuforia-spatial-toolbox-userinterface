65f5fcb28a5903ef393f3dc6096c0ea79e1d692a
Ben Reynolds <breynolds@ptc.com>
2018-10-10 16:57:28 -0400

    documents network/realtime.js

--- src/network/realtime.js
+++ src/network/realtime.js
@@ -9,55 +9,106 @@ createNameSpace("realityEditor.network.realtime");
 
     var mySocket;
     var sockets = {};
-    
+
+    /**
+     * Public init function that sets up the sockets for realtime updates.
+     */
     function initFeature() {
         mySocket = io();
         setupVehicleUpdateSockets();
-        
         setupServerSockets();
         setInterval(setupServerSockets, 3000);
     }
-    
+
+    /**
+     * Add socket listeners for events that update objects, frames, and nodes.
+     */
     function setupVehicleUpdateSockets() {
-        
         addSocketMessageListener('/update/object', updateObject);
-
         addSocketMessageListener('/update/frame', updateFrame);
-
         addSocketMessageListener('/update/node', updateNode);
-
-        // addSocketMessageListener('/update', function(msgContent) {
-        // // socket.on('/update', function(msg) {
-        //     var objectKey;
-        //     var frameKey;
-        //     var nodeKey;
-        //
-        //     // var msgContent = JSON.parse(msg);
-        //     if (typeof msgContent.objectKey !== 'undefined') {
-        //         objectKey = msgContent.objectKey;
-        //     }
-        //     if (typeof msgContent.frameKey !== 'undefined') {
-        //         frameKey = msgContent.frameKey;
-        //     }
-        //     if (typeof msgContent.nodeKey !== 'undefined') {
-        //         nodeKey = msgContent.nodeKey;
-        //     }
-        //
-        //     if (objectKey && frameKey && nodeKey) {
-        //         // io.emit('/update/node', msgContent);
-        //         updateNode(msgContent);
-        //     } else if (objectKey && frameKey) {
-        //         // io.emit('/update/frame', msgContent);
-        //         updateFrame(msgContent);
-        //     } else if (objectKey) {
-        //         // io.emit('/update/object', msgContent);
-        //         updateObject(msgContent);
-        //     }
-        //
-        // });
-        
     }
-    
+
+    /**
+     * @typedef {Object} UpdateMessage
+     * @desc A structured JSON message with information to update a specific property of an object, frame, or node
+     * @property {string} objectKey
+     * @property {string|undefined} frameKey
+     * @property {string|undefined} nodeKey
+     * @property {string} propertyPath
+     * @property {*} newValue
+     * @property {string} editorId - the uuid of the editor that sent the message
+     */
+
+    /**
+     * Updates a specific property of a specific object to a new value.
+     * @param {UpdateMessage} msgContent
+     */
+    function updateObject(msgContent) {
+        var object = realityEditor.getObject(msgContent.objectKey);
+        if (!object) { return; }
+        if (!msgContent.hasOwnProperty('propertyPath') || !msgContent.hasOwnProperty('newValue')) { return; }
+
+        setObjectValueAtPath(object, msgContent.propertyPath, msgContent.newValue);
+        console.log('set object (' + msgContent.objectKey + ').' + msgContent.propertyPath + ' to ' + msgContent.newValue);
+    }
+
+    /**
+     * Updates a specific property of a specific frame to a new value.
+     * @param {UpdateMessage} msgContent
+     */
+    function updateFrame(msgContent) {
+        var frame = realityEditor.getFrame(msgContent.objectKey, msgContent.frameKey);
+        if (!frame) { return; }
+        if (!msgContent.hasOwnProperty('propertyPath') || !msgContent.hasOwnProperty('newValue')) { return; }
+
+        setObjectValueAtPath(frame, msgContent.propertyPath, msgContent.newValue);
+        console.log('set frame (' + msgContent.frameKey + ').' + msgContent.propertyPath + ' to ' + msgContent.newValue);
+    }
+
+    /**
+     * Updates a specific property of a specific node to a new value.
+     * @param {UpdateMessage} msgContent
+     */
+    function updateNode(msgContent) {
+        var node = realityEditor.getNode(msgContent.objectKey, msgContent.frameKey, msgContent.nodeKey);
+        if (!node) { return; }
+        if (!msgContent.hasOwnProperty('propertyPath') || !msgContent.hasOwnProperty('newValue')) { return; }
+
+        setObjectValueAtPath(node, msgContent.propertyPath, msgContent.newValue);
+        console.log('set node (' + msgContent.nodeKey + ').' + msgContent.propertyPath + ' to ' + msgContent.newValue);
+    }
+
+    /**
+     * Checks every object detected, and if there are any belonging to servers that we haven't established a socket
+     * connection with, adds a new 'realityServers' socket connection
+     */
+    function setupServerSockets() {
+        var serverPort = 8080;
+        var ipList = [];
+        realityEditor.forEachObject(function(object, objectKey) {
+            if (ipList.indexOf(object.ip) === -1) {
+                ipList.push(object.ip);
+            }
+        });
+        ipList.forEach(function(ip) {
+            var serverAddress = 'http://' + ip + ':' + serverPort;
+            var socketsIps = realityEditor.network.realtime.getSocketIPsForSet('realityServers');
+            if (socketsIps.indexOf(serverAddress) < 0) {
+                // if we haven't already created a socket connection to that IP, create a new one,
+                //   and register update listeners, and emit a /subscribe message so it can connect back to us
+                realityEditor.network.realtime.createSocketInSet('realityServers', serverAddress);
+                sockets['realityServers'][serverAddress].emit('/subscribe/realityEditorUpdates', JSON.stringify({editorId: globalStates.tempUuid}));
+                addServerUpdateListener(serverAddress);
+            }
+        });
+    }
+
+    /**
+     * Adds a generic '/update' listener to the socket.io server running at the provided address, and delegates the
+     * responses to updateObject, updateFrame, or updateNode depending on the messages received
+     * @param {string} serverAddress
+     */
     function addServerUpdateListener(serverAddress) {
         addServerSocketMessageListener(serverAddress, '/update', function(msg) {
             // socket.on('/update', function(msg) {
@@ -77,74 +128,44 @@ createNameSpace("realityEditor.network.realtime");
             }
 
             if (objectKey && frameKey && nodeKey) {
-                // io.emit('/update/node', msgContent);
                 updateNode(msgContent);
             } else if (objectKey && frameKey) {
-                // io.emit('/update/frame', msgContent);
                 updateFrame(msgContent);
             } else if (objectKey) {
-                // io.emit('/update/object', msgContent);
                 updateObject(msgContent);
             }
 
         });
     }
-    
-    function updateObject(msgContent) {
-        var object = realityEditor.getObject(msgContent.objectKey);
-        if (!object) { return; }
-        if (!msgContent.hasOwnProperty('propertyPath') || !msgContent.hasOwnProperty('newValue')) { return; }
 
-        setObjectValueAtPath(object, msgContent.propertyPath, msgContent.newValue);
-        console.log('set object (' + msgContent.objectKey + ').' + msgContent.propertyPath + ' to ' + msgContent.newValue);
-    }
-    
-    function updateFrame(msgContent) {
-        var frame = realityEditor.getFrame(msgContent.objectKey, msgContent.frameKey);
-        if (!frame) { return; }
-        if (!msgContent.hasOwnProperty('propertyPath') || !msgContent.hasOwnProperty('newValue')) { return; }
-
-        setObjectValueAtPath(frame, msgContent.propertyPath, msgContent.newValue);
-        console.log('set frame (' + msgContent.frameKey + ').' + msgContent.propertyPath + ' to ' + msgContent.newValue);
-    }
-    
-    function updateNode(msgContent) {
-        var node = realityEditor.getNode(msgContent.objectKey, msgContent.frameKey, msgContent.nodeKey);
-        if (!node) { return; }
-        if (!msgContent.hasOwnProperty('propertyPath') || !msgContent.hasOwnProperty('newValue')) { return; }
-
-        setObjectValueAtPath(node, msgContent.propertyPath, msgContent.newValue);
-        console.log('set node (' + msgContent.nodeKey + ').' + msgContent.propertyPath + ' to ' + msgContent.newValue);
-    }
-    
-    function setupServerSockets() {
-        var serverPort = 8080;
-        var ipList = [];
-        realityEditor.forEachObject(function(object, objectKey) {
-            if (ipList.indexOf(object.ip) === -1) {
-                ipList.push(object.ip);
-            }
-        });
-        ipList.forEach(function(ip) {
-            var serverAddress = 'http://' + ip + ':' + serverPort;
-            var socketsIps = realityEditor.network.realtime.getSocketIPsForSet('realityServers');
-            if (socketsIps.indexOf(serverAddress) < 0) {
-                realityEditor.network.realtime.createSocketInSet('realityServers', serverAddress);
-                sockets['realityServers'][serverAddress].emit('/subscribe/realityEditorUpdates', JSON.stringify({editorId: globalStates.tempUuid}));
-                addServerUpdateListener(serverAddress);
-            }
-        });
-    }
-    
+    /**
+     * Utility function that abstracts adding a message listener on this server's socket.
+     * @param {string} messageName
+     * @param {function} callback
+     */
     function addSocketMessageListener(messageName, callback) {
         mySocket.on(messageName, callback);
     }
-    
+
+    /**
+     * Utility function that abstracts adding a message on the socket to the specified serverAddress.
+     * @param {string} serverAddress
+     * @param {string} messageName
+     * @param {function} callback
+     */
     function addServerSocketMessageListener(serverAddress, messageName, callback) {
         sockets['realityServers'][serverAddress].on(messageName, callback);
     }
 
-    // TODO: control which sets it gets sent to
+    /**
+     * Sends a socket message to all connected realityServers sockets, with instructions to update a specific property
+     *   of an object, frame, or node
+     * @param {string} objectKey
+     * @param {string} frameKey
+     * @param {string} nodeKey
+     * @param {string} propertyPath
+     * @param {*} newValue
+     */
     function broadcastUpdate(objectKey, frameKey, nodeKey, propertyPath, newValue) {
         
         var messageBody = {
@@ -155,30 +176,38 @@ createNameSpace("realityEditor.network.realtime");
             newValue: newValue,
             editorId: globalStates.tempUuid
         };
-
-        // var setNames = Object.keys(sockets);
-        // setNames.forEach(function(setName) {
-        //     sendMessageToSocketSet(setName, '/update', messageBody);
-        // });
         
         sendMessageToSocketSet('realityServers', '/update', messageBody);
-        
     }
-    
+
+    /**
+     * Creates a set where a number of socket connections can be organized within the same namespace.
+     *  e.g. so you can send different messages to different types of sockets
+     * @param {string} setName
+     */
     function createSocketSet(setName) {
         if (typeof sockets[setName] === 'undefined') {
             sockets[setName] = {};
         }
     }
-    
+
+    /**
+     * Utility that returns the IDs of all sockets in a given set.
+     * @param {string} setName
+     * @return {Array.<string>}
+     */
     function getSocketIPsForSet(setName) {
         if (typeof sockets[setName] === 'undefined') {
             return [];
         }
-        
         return Object.keys(sockets[setName]);
     }
-    
+
+    /**
+     * Creates a new socket in the specified set. Creates the set if it doesn't already exist.
+     * @param {string} setName
+     * @param {string} socketIP
+     */
     function createSocketInSet(setName, socketIP) {
         var ioObject = io.connect(socketIP);
         console.log(ioObject);
@@ -186,7 +215,13 @@ createNameSpace("realityEditor.network.realtime");
         sockets[setName][socketIP] = ioObject;
         console.log('created [' + setName + '] socket to IP: ' + socketIP);
     }
-    
+
+    /**
+     * Sends the eventName and messageBody to every socket connection in the specified set.
+     * @param {string} setName
+     * @param {string} eventName
+     * @param {Object} messageBody
+     */
     function sendMessageToSocketSet(setName, eventName, messageBody) {
         var socketIPs = getSocketIPsForSet(setName);
         socketIPs.forEach(function(socketIP) {
