8c5e63b8692bbd38d6d9f9edfbd08b1c511df898
Ben Reynolds <breynolds@ptc.com>
2019-08-26 10:36:47 -0400

    removed screenshot frame and fixed node positions for videoRecording frame

--- src/device/videoRecording.js
+++ src/device/videoRecording.js
@@ -156,8 +156,8 @@ createNameSpace("realityEditor.device.videoRecording");
         var videoPath = 'http://' + object.ip + ':' + httpPort + '/obj/' + object.name + '/videos/' + videoId + '.mp4';
 
         var nodes = [
-            {name: 'play', type: 'node'},
-            {name: 'progress', type: 'node'},
+            {name: 'play', type: 'node', x: 40, y: 0},
+            {name: 'progress', type: 'node', x: -40, y: 0},
             {name: 'storage', type: 'storeData', publicData: {data: videoPath}}
         ];
         
@@ -176,8 +176,8 @@ createNameSpace("realityEditor.device.videoRecording");
             addedNode.type = nodeType;
             addedNode.frameSizeX = 220;
             addedNode.frameSizeY = 220;
-            addedNode.x = realityEditor.device.utilities.randomIntInc(0, 200) - 100;
-            addedNode.y = realityEditor.device.utilities.randomIntInc(0, 200) - 100;
+            addedNode.x = nodeData.x || 0; //realityEditor.device.utilities.randomIntInc(0, 200) - 100;
+            addedNode.y = nodeData.y || 0; //realityEditor.device.utilities.randomIntInc(0, 200) - 100;
             addedNode.scale = globalStates.defaultScale;
             
             if (typeof nodeData.publicData !== 'undefined') {
