91b9df8bf4e52ac5a8901efeab01f3c641d5e4cc
Ben Reynolds <breynolds@ptc.com>
2019-09-12 17:01:24 -0400

    cleans up some global variables from src/states by deleting a couple that arent used anymore and moving some others to the module they are associated with. src/states still needs a lot of refactoring to get rid of unnecessary global variables

--- src/states.js
+++ src/states.js
@@ -51,8 +51,6 @@
  ******************************************** constant settings *******************************************************
  **********************************************************************************************************************/
 
-var disp = {};
-var uiButtons;
 var httpPort = 8080;
 var timeForContentLoaded = 100; // temporary set to 10000 with the UI Recording mode for video recording
 var timeCorrection = {delta: 0, now: 0, then: 0};
@@ -70,7 +68,6 @@ var globalStates = {
 	tempUuid : "0000",
 	debug: false,
     debugSpeechConsole: false,
-	overlay: 0,
 	device: "",
 	// drawWithLines
 	ballDistance: 14,
@@ -184,15 +181,6 @@ var globalStates = {
 
 var globalCanvas = {};
 
-var globalLogic ={
-	size:0,
-	x:0,
-	y:0,
-	rectPoints: [],
-	farFrontElement:"",
-	frontDepth: 1000000
-};
-
 var publicDataCache = {};
 
 // reconstructs the grouping structure on the UI side
@@ -260,41 +248,6 @@ var globalProgram = {
 	logicB:false,
 	logicSelector:4
 };
-var globalMatrix = {
-	temp: [
-		1, 0, 0, 0,
-		0, 1, 0, 0,
-		0, 0, 1, 0,
-		0, 0, 0, 1
-	],
-	begin: [
-		1, 0, 0, 0,
-		0, 1, 0, 0,
-		0, 0, 1, 0,
-		0, 0, 0, 1
-	],
-	end: [
-		1, 0, 0, 0,
-		0, 1, 0, 0,
-		0, 0, 1, 0,
-		0, 0, 0, 1
-	],
-	r: [
-		1, 0, 0, 0,
-		0, 1, 0, 0,
-		0, 0, 1, 0,
-		0, 0, 0, 1
-	],
-	r2: [
-		1, 0, 0, 0,
-		0, 1, 0, 0,
-		0, 0, 1, 0,
-		0, 0, 0, 1
-	],
-	copyStillFromMatrixSwitch: false
-};
-
-var consoleText = "";
 
 var rotateX = [
     1, 0, 0, 0,
@@ -302,7 +255,6 @@ var rotateX = [
     0, 0, 1, 0,
     0, 0, 0, 1
 ];
-
 
 var makeRotationX =  function ( theta ) {
 
@@ -314,7 +266,7 @@ var makeRotationX =  function ( theta ) {
         0, 0, 0, 1];
 };
 
-var rotationXMartrix =  makeRotationX(-(Math.PI/2));
+var rotationXMatrix =  makeRotationX(-(Math.PI/2));
 
 var editingAnimationsMatrix = [
     1, 0, 0, 0,
@@ -325,39 +277,16 @@ var editingAnimationsMatrix = [
 
 var pocketDropAnimation = null;
 
-// var pocketBegin = [1137.549909421903,12.017532798048029,-0.03482891256371417,-0.03475932439627283,-11.812648290367441,1137.738228161505,0.005875104883220343,0.005863366423640215,-22.38728737682625,11.161969977619812, -2.003692935114902, -1.9996895566225437, -9437.22693164777, 6368.974843939889, 793.5453413849068, 795.955841789644];
-
+// pocketBegin is a hard-coded value that places a new frame in a nice spot relative to the camera when it spawns
 var pocketBegin = [1138.6973538592133,13.991065713633931,0.06988535562447923,0.06993400512601616,-16.11745909667903,1136.2485938902328,0.08978962416316712,0.08976110615564072,138.96584170397102,73.32506513216471,-2.5095775949204446,-2.505473059727466,34327.448087541634,-2445.6806878581046,428.02424166124865,430.5078316680102];
-
+// old value: [1137.549909421903,12.017532798048029,-0.03482891256371417,-0.03475932439627283,-11.812648290367441,1137.738228161505,0.005875104883220343,0.005863366423640215,-22.38728737682625,11.161969977619812, -2.003692935114902, -1.9996895566225437, -9437.22693164777, 6368.974843939889, 793.5453413849068, 795.955841789644]
 
 var visibleObjectTapInterval = null;
 var visibleObjectTapDelay = 1000;
 
-var testInterlink = {};
-
 var overlayDiv;
-//var overlayImg;
-//var overlayImage = [];
 
 var CRAFTING_GRID_WIDTH = 506;
 var CRAFTING_GRID_HEIGHT = 320;
 
-/**********************************************************************************************************************
- ***************************************** datacrafting variables  ****************************************************
- **********************************************************************************************************************/
-
-// var blockColorMap = {
-//    bright:["#2DFFFE", "#29FD2F", "#FFFD38", "#FC157D"], // blue, green, yellow, red
-//    faded:["#EAFFFF", "#EAFFEB", "#FFFFEB", "#FFE8F2"] // lighter: blue, green, yellow, red
-//}
-
-var menuBarWidth = 62;
-var blockColorMap = ["#00FFFF", "#00FF00", "#FFFF00", "#FF007C"];
-var columnHighlightColorMap = ["rgba(0,255,255,0.15)", "rgba(0,255,0,0.15)", "rgba(255,255,0,0.15)", "rgba(255,0,124,0.15)"];
-//var activeBlockColor = "#E6E6E6"; // added blocks are grey
-//var movingBlockColor = "#FFFFFF"; // blocks turn white when you start to drag them
-
 var DEBUG_DATACRAFTING = false; // when TRUE -> shows crafting board just by tapping on first menu item (DEBUG mode)
-
-var blockIconCache = {};
-
