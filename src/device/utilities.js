a9abef22539ee871779e590902ce5a1b7826ee31
James Hobin <jhobin@ptc.com>
2019-09-17 12:20:21 -0400

    Mark exported variables

--- src/device/utilities.js
+++ src/device/utilities.js
@@ -47,6 +47,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
+/* exported cout */
 
 /**
  * Created by heun on 12/27/16.
--- src/gui/pocketFrames.js
+++ src/gui/pocketFrames.js
@@ -1,3 +1,5 @@
+/* exported realityElements */
+
 var realityElements = [
 /*
     {
