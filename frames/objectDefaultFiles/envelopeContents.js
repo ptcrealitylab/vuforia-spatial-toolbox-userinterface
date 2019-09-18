1583ffba6fd85bc557f5c3f30d8da4cbe0d29073
Ben Reynolds <breynolds@ptc.com>
2019-09-18 15:31:21 -0400

    adding more skeleton code to Envelope and EnvelopeContents classes for designating frames as a container of other frames, or as something that can be contained. provides some simple APIs for keeping track of all frames in one of these envelope groups, managing their order, and subscribing to events and message-passing that can be implemented to update their UI

---
+++ frames/objectDefaultFiles/envelopeContents.js
@@ -0,0 +1,99 @@
+(function(exports) {
+
+    /* eslint no-inner-declarations: "off" */
+    // makes sure this only gets loaded once per iframe
+    if (typeof exports.EnvelopeContents !== 'undefined') {
+        return;
+    }
+
+    /**
+     * @constructor
+     */
+    function EnvelopeContents(realityInterface) {
+        this.realityInterface = realityInterface;
+        // this.isOrdered = false;
+        // this.orderIndex = -1;
+        // this.totalNumberOfFramesInEnvelope = 1;
+        /**
+         * Callbacks for various events from contained frames or the reality editor
+         * @type {{onFrameAdded: null, onFrameDeleted: null, onMessageFromFrame: null, onOpen: null, onClose: null}}
+         */
+        this.callbacks = {
+            /**
+             * Triggered when a the frame's order changes for any reason
+             */
+            onOrderUpdated: [],
+            /**
+             * Triggered when another contained frame sends a message to this one (e.g. "stepCompleted")
+             */
+            onMessageFromFrame: [],
+            /**
+             * Triggered when the envelope frame sends a message directly to this one
+             */
+            onMessageFromEnvelope: []
+        };
+
+        /**
+         * Triggers all callbacks functions when the iframe receives an 'envelopeMessage' POST message from the parent window.
+         * It is the responsibility of each callback function to filter out messages that aren't directed to it.
+         */
+        window.addEventListener('message', function (msg) {
+            let msgContent = JSON.parse(msg.data);
+            if (typeof msgContent.envelopeMessage === 'undefined') {
+                return;
+            }
+            for (let callbackKey in this.callbacks) {
+                if (this.callbacks[callbackKey]) { // only trigger for callbacks that have been set
+                    this.callbacks[callbackKey].forEach(function(addedCallback) {
+                        addedCallback(msgContent.envelopeMessage)
+                    });
+                }
+            }
+        }.bind(this));
+
+        // // this keeps the list of contained frames and the ordering up-to-date
+        // this.onOrderUpdated(function(orderUpdatedMessage) {
+        //     this.orderIndex = orderUpdatedMessage.index;
+        //     this.totalNumberOfFramesInEnvelope = orderUpdatedMessage.total;
+        // }.bind(this));
+
+    }
+
+    //
+    // Methods to subscribe to events from contained frames or from the reality editor 
+    //
+
+    EnvelopeContents.prototype.onMessageFromFrame = function(callback) {
+        this.addCallback('onMessageFromFrame', callback);
+    };
+
+    EnvelopeContents.prototype.onMessageFromEnvelope = function(callback) {
+        this.addCallback('onMessageFromEnvelope', callback);
+    };
+    
+    EnvelopeContents.prototype.onOrderUpdated = function(callback) {
+        this.addCallback('onOrderUpdated', callback);
+    };
+
+    /**
+     * Helper function to correctly add a callback function
+     * @param {string} callbackName - should match one of the keys in this.callbacks
+     * @param {function} callbackFunction
+     */
+    EnvelopeContents.prototype.addCallback = function(callbackName, callbackFunction) {
+        if (typeof this.callbacks[callbackName] === 'undefined') {
+            console.warn('Creating a new envelope callback that wasn\'t defined in the constructor');
+            this.callbacks[callbackName] = [];
+        }
+
+        this.callbacks[callbackName].push(function(envelopeMessage) {
+            if (typeof envelopeMessage[callbackName] === 'undefined') { return; }
+            callbackFunction(envelopeMessage[callbackName]);
+        });
+    };
+
+    exports.EnvelopeContents = EnvelopeContents;
+    
+})(window);
+
+
