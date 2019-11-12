5b9bb16cfbf73b8e9e2d3bb8ad61a9270b85c79d
Ben Reynolds <breynolds@ptc.com>
2019-11-12 09:30:42 -0500

    adds new camera mode similar to Cinema4D, where you move a virtual point around that you can orbit around, but which can snap to objects

--- src/states.js
+++ src/states.js
@@ -69,7 +69,7 @@ var globalStates = {
     craftingMoveDelay : 400,
     tempUuid : "0000",
     debug: false,
-    debugSpeechConsole: true,
+    debugSpeechConsole: false,
     device: "",
     // drawWithLines
     ballDistance: 14,
