05a09e3d72be6da8428112ab90f725f954f91539
Ben Reynolds <breynolds@ptc.com>
2020-09-30 12:21:05 -0400

    adds first pseudocode implementation of new render code

--- src/gui/ar/sceneRenderer.js
+++ src/gui/ar/sceneRenderer.js
@@ -16,6 +16,136 @@ createNameSpace("realityEditor.gui.ar.sceneRenderer");
 (function(exports) {
     let previousVisibleObjects = [];
     const elementCache = {};
+    
+    function update_pseudocode(visibleObjects) {
+        // miscellaneous things to happen each update, possibly changing visibleObjects before rendering
+        pre_update(visibleObjects);
+        
+        // update the scene graph
+        realityEditor.gui.ar.sceneGraph.calculateFinalMatrices(Object.keys(visibleObjects));
+
+        // this will recursively update each frame in the object
+        // and each node in each frame
+        forEachVisibleObject(visibleObjects, function(objectKey, object) {
+            update_object(objectKey, object);
+        });
+        
+        forEachNonVisibleObject(visibleObjects, function(objectKey, object) {
+            // if it was previously visible, do a one-time update
+            
+            // if it wasn't previously visible, kill if hidden for enough time
+        });
+        
+        post_update(visibleObjects);
+    }
+    
+    function pre_update(visibleObjects) {
+        // update timer to drive system-wide animations this frame
+        realityEditor.gui.ar.utilities.timeSynchronizer(timeCorrection);
+
+        // allow other modules to modify the set of objects currently seen (except while frozen)
+        if (!globalStates.freezeButtonState) {
+            this.visibleObjectModifiers.forEach(function(callback) {
+                callback(visibleObjects);
+            });
+        }
+
+        // erases anything on the background canvas
+        if (globalCanvas.hasContent === true) {
+            globalCanvas.context.clearRect(0, 0, globalCanvas.canvas.width, globalCanvas.canvas.height);
+            globalCanvas.hasContent = false;
+        }
+
+        // make sure that all Spatial Questions are empty
+        realityEditor.gui.spatial.clearSpatialList();
+        
+        // TODO: if object contains sticky frame and not in visible objects, add it to visible objects
+        
+        // TODO: add low-frequency updates
+        
+        // TODO: possibly add pulsing haptic feedback when looking at object with no visible frames
+    }
+    
+    function post_update(visibleObjects) {
+        // draw all links and lines
+        if (globalStates.guiState === "logic") {
+            realityEditor.gui.crafting.redrawDataCrafting();  // todo maybe animation frame
+        }
+        
+        // render transition frame
+        // provide haptic feedback
+        // trigger closestObject and update listeners callbacks
+    }
+
+    function update_object(objectKey, object) {
+        // possibly ignore world objects before localized or process differently?
+        
+        // for non-world objects, collectSpatialList
+        
+        // update each frame in this object
+        Object.keys(object.frames).forEach(function(frameKey) {
+            let frame = realityEditor.getFrame(objectKey, frameKey);
+            if (frame) {
+                update_frame(objectKey, frameKey, object, frame);
+            }
+        });
+    }
+    
+    function update_frame(objectKey, frameKey, object, frame) {
+        // filter out frame if it meets certain criteria (visualization=screen)
+        // update CSS classes based on ignoreAllTouches API/property?
+        
+        // draw frame (if in ui mode or semi-transparent if in node mode)
+        // ... make DOM elements visible if needed
+        
+        // update opacity based on distance
+        
+        // lots of random adjustments for initial placement, pocket
+        // (scenegraph needs to take ground plane into account so we dont change here)
+        // include animation matrix? possibly? or actually animate position in scene graph?
+        
+        // calculate isNowOutsideViewport from canUnload to unload/reload iframes
+        
+        // update CSS classes for stickyFullscreen edge cases?
+        
+        // check if pulled into unconstrained?
+        
+        // compile all requested matrices for tool API subscriptions and post into iframe
+        
+        // update each node in this frame
+        Object.keys(frame.nodes).forEach(function(nodeKey) {
+            let node = realityEditor.getNode(objectKey, frameKey, nodeKey);
+            if (node) {
+                update_node(objectKey, frameKey, object, frame);
+            }
+        });
+    }
+
+    function update_node(objectKey, frameKey, nodeKey, object, frame, node) {
+        // draw node (same subroutine as update_frame)
+        // except filter out nodes that are in hiddenNodeTypes instead of visualization
+        
+        // animate logic node contents based on touch position?
+    }
+
+    function forEachVisibleObject(visibleObjects, callback) {
+        Object.keys(visibleObjects).forEach(function(objectKey) {
+            let object = realityEditor.getObject(objectKey);
+            if (object) {
+                callback(objectKey, object);
+            }
+        });
+    }
+
+    function forEachNonVisibleObject(visibleObjects, callback) {
+        Object.keys(objects).forEach(function(objectKey) {
+            if (typeof visibleObjects[objectKey] !== 'undefined') { return; }
+            let object = realityEditor.getObject(objectKey);
+            if (object) {
+                callback(objectKey, object);
+            }
+        })
+    }
 
     function draw(visibleObjects) {
 
@@ -153,4 +283,6 @@ createNameSpace("realityEditor.gui.ar.sceneRenderer");
     }
 
     exports.draw = draw;
+    
+    exports.update_pseudocode = update_pseudocode;
 })(realityEditor.gui.ar.sceneRenderer);
