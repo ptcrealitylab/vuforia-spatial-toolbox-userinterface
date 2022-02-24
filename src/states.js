1a0ad1052182a9b0dadfe945808b88ad2e79f8fd
James Hobin <jhobin@ptc.com>
2022-02-24 15:48:42 -0500

    Make acceptUDPBeats a scoped variable

--- src/states.js
+++ src/states.js
@@ -58,8 +58,8 @@ var defaultHttpPort = 8080;
 var timeForContentLoaded = 100; // temporary set to 10000 with the UI Recording mode for video recording
 var timeCorrection = {delta: 0, now: 0, then: 0};
 var boundListeners = {};
-var acceptUDPBeats = true;
-var updateFramerate = 20; // fps
+var printLog = false;
+let updateFramerate = 20; // fps
 var TEMP_DISABLE_MEMORIES = false;
 
 // noinspection JSSuspiciousNameCombination - (width is based on innerHeight and vice versa)
