4989450771603aa2694908a2ee9fca125d79b75b
benptc <breynolds@ptc.com>
2022-10-24 13:17:36 -0400

    createFrame now adds the tools at the spatialCursor position, with a series of transformations to ensure they are rotated for the best viewing angle (upright and facing the camera as much as possible while still snapping to the plane of the cursor)

--- src/gui/ar/utilities.js
+++ src/gui/ar/utilities.js
@@ -1455,5 +1455,26 @@ function polyfillWebkitConvertPointFromPageToNode() {
         return A[0] * B[0] + A[1] * B[1] + A[2] * B[2];
     }
 
+    function getRight(M) {
+        return normalize([M[0], M[1], M[2]]);
+    }
+
+    function getUp(M) {
+        return normalize([M[4], M[5], M[6]]);
+    }
+
+    function getForward(M) {
+        return normalize([M[8], M[9], M[10]]);
+    }
+
     exports.lookAt = lookAt;
+    exports.negate = negate;
+    exports.add = add;
+    exports.magnitude = magnitude;
+    exports.normalize = normalize;
+    exports.crossProduct = crossProduct;
+    exports.dotProduct = dotProduct;
+    exports.getRight = getRight;
+    exports.getUp = getUp;
+    exports.getForward = getForward;
 })(realityEditor.gui.ar.utilities);
