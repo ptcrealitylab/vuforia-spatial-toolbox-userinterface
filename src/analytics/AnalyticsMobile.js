92a04cc58484398fc970ac1500afc1f17cb1fb80
James Hobin <jhobin@ptc.com>
2023-03-01 16:00:31 -0500

    Fix typo; Perform postPersistRequest on mobile

--- src/analytics/AnalyticsMobile.js
+++ src/analytics/AnalyticsMobile.js
@@ -1,5 +1,10 @@
-export class AnalyticsMock {
+import {
+    postPersistRequest,
+} from './utils.js';
+
+export class AnalyticsMobile {
     constructor() {
+        this.livePlayback = false;
     }
 
     add() {
@@ -20,7 +25,16 @@ export class AnalyticsMock {
     setHighlightRegion() {
     }
 
-    setDisplayRegion() {
+    /**
+     * @param {{startTime: number, endTime: number}} region
+     * @param {boolean} _fromSpaghetti - unused
+     */
+    async setDisplayRegion(region, _fromSpaghetti) {
+        let livePlayback = region.startTime < 0 || region.endTime < 0;
+        if (this.livePlayback && !livePlayback) {
+            await postPersistRequest();
+        }
+        this.livePlayback = livePlayback;
     }
 
     hydrateRegionCards() {
