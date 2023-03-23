155124f614038ad8c0d8058359c116064bbf49ba
James Hobin <jhobin@ptc.com>
2023-03-23 10:38:54 -0400

    Make getPosesInTimeInterval default to live instead of all

--- src/humanPose/draw.js
+++ src/humanPose/draw.js
@@ -658,7 +658,7 @@ export class HumanPoseAnalyzer {
      * @return {Pose[]} - all poses in the time interval
      */
     getPosesInTimeInterval(firstTimestamp, secondTimestamp) {
-        let clonesList = this.clones.all;
+        let clonesList = this.clones.live;
         if (this.clones.historical.length >= this.clones.live.length) {
             clonesList = this.clones.historical;
         }
