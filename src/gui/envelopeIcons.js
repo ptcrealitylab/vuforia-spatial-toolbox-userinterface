99188df46413ddaf25302c96c12c4db82268d77b
benptc <breynolds@ptc.com>
2023-04-11 13:25:13 -0400

    cleanup envelope icon renderer

--- src/gui/envelopeIcons.js
+++ src/gui/envelopeIcons.js
@@ -1,7 +1,23 @@
+/**
+ * This is used to render temporarily icons for envelopes that have received a
+ * "blur" event to remove their 2D UI layer but keep their 3D fullscreen content
+ * on the screen; we add simple image divs floating at the envelope origin so
+ * that clicking on them can restore "focus" to that envelope.
+ */
 class EnvelopeIconRenderer {
     constructor() {
         this.knownEnvelopes = {};
         this.arUtilities = realityEditor.gui.ar.utilities;
+    }
+
+    initService() {
+        this.gui = document.getElementById('GUI');
+
+        realityEditor.gui.ar.draw.addUpdateListener(() => {
+            Object.values(this.knownEnvelopes).forEach(envelope => {
+                this.updateEnvelope(envelope);
+            });
+        });
     }
 
     onEnvelopeRegistered(envelope) {
@@ -26,16 +42,6 @@ class EnvelopeIconRenderer {
         this.knownEnvelopes[envelope.frame].hasFocus = false;
     }
 
-    initService() {
-        this.gui = document.getElementById('GUI');
-
-        realityEditor.gui.ar.draw.addUpdateListener(() => {
-            Object.values(this.knownEnvelopes).forEach(envelope => {
-                this.updateEnvelope(envelope);
-            });
-        });
-    }
-
     updateEnvelope(envelope) {
         if (envelope.isOpen && !envelope.hasFocus) {
             this.renderEnvelopeIcon(envelope.object, envelope.frame);
@@ -52,31 +58,26 @@ class EnvelopeIconRenderer {
 
     renderEnvelopeIcon(objectId, frameId) {
         // lazily instantiate the envelope icon if it doesn't already exist
-        let iconDiv = this.getIconDiv(frameId);
+        let iconDiv = globalDOMCache['envelopeIcon_' + frameId];
         let frame = realityEditor.getFrame(objectId, frameId);
         if (!iconDiv) {
             let object = realityEditor.getObject(objectId);
-            let name =  frame.src;
-            let src = realityEditor.network.getURL(object.ip, realityEditor.network.getPort(object), '/frames/' + name + '/icon.gif');
+            let name = frame.src;
+            let port = realityEditor.network.getPort(object);
+            let path = '/frames/' + name + '/icon.gif';
+            let src = realityEditor.network.getURL(object.ip, port, path);
             iconDiv = this.createIconDiv(frameId, src);
         }
 
-        // let finalMatrix = this.arUtilities.copyMatrix(realityEditor.sceneGraph.getCSSMatrix(frameId));
-
         // We ALWAYS want the icon to face the camera, so don't need to check if frame.alwaysFaceCamera is true
-        // if (frame.alwaysFaceCamera === true) {
+        // let finalMatrix = this.arUtilities.copyMatrix(realityEditor.sceneGraph.getCSSMatrix(frameId));
         let finalMatrix = [];
         let modelMatrix = realityEditor.sceneGraph.getModelMatrixLookingAt(frameId, 'CAMERA');
         let modelViewMatrix = [];
         this.arUtilities.multiplyMatrix(modelMatrix, realityEditor.sceneGraph.getViewMatrix(), modelViewMatrix);
         this.arUtilities.multiplyMatrix(modelViewMatrix, globalStates.projectionMatrix, finalMatrix);
-        // }
 
         iconDiv.style.transform = 'matrix3d(' + finalMatrix.toString() + ')';
-    }
-
-    getIconDiv(frameId) {
-        return globalDOMCache['envelopeIcon_' + frameId];
     }
 
     createIconDiv(frameId, src) {
@@ -85,28 +86,24 @@ class EnvelopeIconRenderer {
         container.classList.add('main', 'visibleFrameContainer');
         container.style.width = '100vw';
         container.style.height = '100vh';
+        this.gui.appendChild(container);
 
         let icon = document.createElement('img');
         icon.src = src;
-
-        let iconWidth = 440;
-        let borderWidth = 16;
+        let iconWidth = 440, borderWidth = 16;
         icon.style.position = 'absolute';
         icon.style.width = `${iconWidth}px`;
         icon.style.height = `${iconWidth}px`;
         icon.style.left = `calc(100vw/2 - ${iconWidth}px/2 - ${borderWidth}px)`;
         icon.style.top = `calc(100vh/2 - ${iconWidth}px/2 - ${borderWidth}px)`;
-
         icon.style.border = `${borderWidth}px solid white`;
         icon.style.borderRadius = '96px';
-
         container.appendChild(icon);
 
         icon.addEventListener('pointerup', () => {
             realityEditor.envelopeManager.focusEnvelope(frameId);
         });
 
-        this.gui.appendChild(container);
         globalDOMCache['envelopeIcon_' + frameId] = container;
         return container;
     }
