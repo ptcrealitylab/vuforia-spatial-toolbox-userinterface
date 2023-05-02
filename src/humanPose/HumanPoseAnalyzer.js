0dd72cc27b96d2c310f8046310728cd2aeab3235
James Hobin <jhobin@ptc.com>
2023-05-02 12:28:46 -0400

    Further raise base opacity

--- src/humanPose/HumanPoseAnalyzer.js
+++ src/humanPose/HumanPoseAnalyzer.js
@@ -14,7 +14,7 @@ import {HumanPoseRenderer} from './HumanPoseRenderer.js';
 import {HumanPoseRenderInstance} from './HumanPoseRenderInstance.js';
 import {MAX_POSE_INSTANCES, MAX_POSE_INSTANCES_MOBILE} from './constants.js';
 
-const POSE_OPACITY_BASE = 0.9;
+const POSE_OPACITY_BASE = 1;
 const POSE_OPACITY_BACKGROUND = 0.2;
 
 /**
