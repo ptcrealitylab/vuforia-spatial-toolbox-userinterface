e1b9945d6ac8037a80f364a38eec495f83a42f4c
benptc <breynolds@ptc.com>
2023-05-31 13:57:11 -0400

    optimize for mobile browser remote operator

--- src/gui/ar/areaCreator.js
+++ src/gui/ar/areaCreator.js
@@ -318,7 +318,13 @@ realityEditor.gui.ar.areaCreator.calculateGroundPlaneIntersection = function(eve
     }
 }
 
+realityEditor.gui.ar.areaCreator.cachedFloorOffset = null;
+
 realityEditor.gui.ar.areaCreator.calculateFloorOffset = function() {
+    if (realityEditor.gui.ar.areaCreator.cachedFloorOffset) {
+        return realityEditor.gui.ar.areaCreator.cachedFloorOffset;
+    }
+
     const worldObjectToolboxMatrix = realityEditor.sceneGraph.getSceneNodeById(realityEditor.sceneGraph.getWorldId()).worldMatrix;
     const worldObjectThreeMatrix = new realityEditor.gui.threejsScene.THREE.Matrix4();
     realityEditor.gui.threejsScene.setMatrixFromArray(worldObjectThreeMatrix, worldObjectToolboxMatrix);
