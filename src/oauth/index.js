ed0ab26d41db6d3bac72f71cac30ee4664bf4aad
Daniel Dangond <ddangond@ptc.com>
2023-07-28 10:22:20 -0400

    Removed unnecessary second loop through all objects

--- src/oauth/index.js
+++ src/oauth/index.js
@@ -12,12 +12,7 @@ import { loadToken } from './tokens.js';
             // The edge server that will handle the processing of the auth code to receive an auth token
             const edgeServer = `http://${object.ip}:${object.port}`; // TODO: more resilient way to do this, not sure if toolbox edge allows connecting via ip:port
 
-            let frameName = frame;
-            realityEditor.forEachFrameInAllObjects((objectKey, frameKey) => { // TODO: make postMessages automatically pass the originating frame to callbacks, make things cleaner here
-                if (frameKey === frame) {
-                    frameName = realityEditor.getFrame(objectKey,frameKey).src;
-                }
-            });
+            let frameName = realityEditor.getFrame(object.objectId,frame).src;
             loadToken(frameName, authorizationUrl, clientId, edgeServer).then(token => {
                 realityEditor.network.postMessageIntoFrame(frame, {
                     onOAuthToken: {
