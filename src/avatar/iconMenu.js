8bcf749649038c3a321ef62055305d48ae604ef1
Ben Reynolds <breynolds@ptc.com>
2024-03-18 10:56:15 -0400

    add LICENSE for splat library, and cleanup iconMenu code

--- src/avatar/iconMenu.js
+++ src/avatar/iconMenu.js
@@ -7,12 +7,18 @@ createNameSpace("realityEditor.avatar.iconMenu");
  */
 
 (function(exports) {
+    // Note: MAX_ICONS isn't set here, it is set to realityEditor.device.environment.variables.maxAvatarIcons
+    // If more than MAX_ICONS avatars are connected, the icons for extras will be hidden/combined in the ellipsis icon
+
+    const ADDITIONAL_NAMES = 2; // list out this many extra names with commas when hovering over the ellipsis
     const ICON_WIDTH = 30; // layout information for circular icons
     const ICON_GAP = 10;
+
     let callbacks = {
-        onAvatarIconClicked: [],
+        onMenuItemClicked: [],
     }
 
+    // Enum of the menu item labels, which other modules can also check against to listen to specific menu items
     const MENU_ITEMS = Object.freeze({
         EditName: 'Edit Name',
         AllFollowMe: 'All Follow Me',
@@ -21,9 +27,10 @@ createNameSpace("realityEditor.avatar.iconMenu");
     });
 
     function initService() {
-        registerAvatarIconClickEvent((params) => {
+        // the iconMenu itself provides the implementation of the Edit Name menu item.
+        // Other menu items can be implemented by their relevant modules, e.g. desktopCamera can handle Follow actions
+        onAvatarIconMenuItemSelected((params) => {
             if (!(params.isMyIcon && params.buttonText === realityEditor.avatar.iconMenu.MENU_ITEMS.EditName)) return;
-
             // show a modal that lets you type in a name
             realityEditor.gui.modal.openInputModal({
                 headerText: 'Edit Avatar Name',
@@ -44,13 +51,19 @@ createNameSpace("realityEditor.avatar.iconMenu");
             });
         });
     }
-
-    // show a list of circular icons, one per avatar, with the (random) color and (chosen) initials of that user
+    /**
+     * Show a list of circular icons, one per avatar, with the (random) color and (chosen) initials of that user.
+     * If too many avatars, combines the overflow into a final icon with (+N) in it.
+     * This uses very simple render logic, which entirely clears the container and rebuilds it each time it renders.
+     * @param {Object.<string, UserProfile>} connectedAvatars
+     */
     function renderAvatarIconList(connectedAvatars) {
         let iconContainer = document.getElementById('avatarIconContainer');
         if (!iconContainer) {
             iconContainer = createIconContainer();
         }
+
+        // reset the container, so we can rebuild it from scratch. simple, inefficient, but reliable.
         while (iconContainer.hasChildNodes()) {
             iconContainer.removeChild(iconContainer.lastChild);
         }
@@ -59,12 +72,13 @@ createNameSpace("realityEditor.avatar.iconMenu");
             return; // don't show unless there is at least one avatar
         }
 
+        // moves you to the front // TODO: sort by recent activity, or who's following you, etc
         let sortedKeys = realityEditor.avatar.utils.sortAvatarList(connectedAvatars);
 
         // if too many collaborators, show a "+N..." at the end (I'm calling this the ellipsis) and limit how many icons
         const MAX_ICONS = realityEditor.device.environment.variables.maxAvatarIcons;
-        const ADDITIONAL_NAMES = 2; // list out this many extra names with commas when hovering over the ellipsis
 
+        // build and add each of the icons to the container, and attach pointer event listeners to them
         sortedKeys.forEach((objectKey, index) => {
             let isEllipsis = index === (MAX_ICONS - 1) && sortedKeys.length > MAX_ICONS; // last one turns into "+2", "+3", etc
             let numTooMany = sortedKeys.length - (MAX_ICONS - 1);
@@ -104,6 +118,7 @@ createNameSpace("realityEditor.avatar.iconMenu");
                 }
             }
 
+            // hovering/clicking on icon image triggers tooltip or dropdown
             let iconImageDiv = iconDiv.querySelector('.avatarListIconImage');
             iconImageDiv.addEventListener('pointerover', () => {
                 showFullNameTooltip(iconImageDiv, tooltipText, isMyIcon, isEllipsis);
@@ -111,20 +126,28 @@ createNameSpace("realityEditor.avatar.iconMenu");
             ['pointerout', 'pointercancel', 'pointerup'].forEach((eventName) => {
                 iconImageDiv.addEventListener(eventName, hideFullNameTooltip);
             });
-
-            iconImageDiv.addEventListener('pointerup', (e) => {
-                toggleDropdown(objectKey, info, initials, isMyIcon, e);
+            iconImageDiv.addEventListener('pointerup', () => {
+                if (isEllipsis) { // don't add follow menu to ellipsis
+                    return; // TODO: what should happen when you click on the "+N" icon? show all names?
+                }
+                toggleDropdown(objectKey, info, initials, isMyIcon);
             });
         });
 
         let iconsWidth = Math.min(MAX_ICONS, sortedKeys.length) * (ICON_WIDTH + ICON_GAP) + ICON_GAP;
         iconContainer.style.width = iconsWidth + 'px';
     }
-
-    function toggleDropdown(objectId, userProfile, userInitials, isMyIcon, pointerEvent) {
+    /**
+     * Shows or hides the dropdown corresponding to the avatar icon for the given avatar objectId
+     * @param {string} objectId
+     * @param {UserProfile} userProfile
+     * @param {string|null} userInitials
+     * @param {boolean} isMyIcon
+     */
+    function toggleDropdown(objectId, userProfile, userInitials, isMyIcon) {
         let iconDropdown = document.getElementById('avatarIconDropdown' + objectId);
         if (!iconDropdown) {
-            iconDropdown = createAvatarIconDropdown(objectId, userProfile, userInitials, isMyIcon, pointerEvent);
+            iconDropdown = createAvatarIconDropdown(objectId, userProfile, userInitials, isMyIcon);
         }
 
         // show or hide the clicked menu depending on previous state
@@ -136,21 +159,32 @@ createNameSpace("realityEditor.avatar.iconMenu");
             hideDropdown(iconDropdown);
         }
     }
-
+    /**
+     * Show the dropdown menu, and hide other dropdowns and tooltips
+     * @param {HTMLElement} iconDropdown
+     */
     function showDropdown(iconDropdown) {
         Array.from(document.querySelectorAll('.avatarListIconDropdown')).forEach(dropdown => {
             hideDropdown(dropdown);
         });
         iconDropdown.classList.remove('hiddenDropdown');
-
-        // hide the hover tooltip, if it's shown
         hideFullNameTooltip();
     }
-
+    /**
+     * Hides a particular dropdown menu
+     * @param {HTMLElement} iconDropdown
+     */
     function hideDropdown(iconDropdown) {
         iconDropdown.classList.add('hiddenDropdown');
     }
-
+    /**
+     * Constructs a dropdown menu with the correct menu items for the specified avatar.
+     * E.g. your own avatar has Edit Name and All Follow Me, while others have Follow and Follow Me
+     * @param {string} objectId
+     * @param {UserProfile} userProfile
+     * @param {string|null} userInitials
+     * @param {boolean} isMyIcon
+     */
     function createAvatarIconDropdown(objectId, userProfile, userInitials, isMyIcon) {
         let parent = document.getElementById('avatarIcon' + objectId);
         if (!parent) {
@@ -170,7 +204,15 @@ createNameSpace("realityEditor.avatar.iconMenu");
         parent.appendChild(container);
         return container;
     }
-
+    /**
+     * Creates a div for the menu item and attaches a pointerup event that other modules can subscribe to
+     * @param {HTMLElement} parentDiv
+     * @param {string} textContent
+     * @param {string} objectId
+     * @param {UserProfile} userProfile
+     * @param {string|null} userInitials
+     * @param {boolean} isMyIcon
+     */
     function addMenuItemToDropdown(parentDiv, textContent, objectId, userProfile, userInitials, isMyIcon) {
         let item = document.createElement('div');
         item.classList.add('avatarListIconDropdownItem');
@@ -178,7 +220,7 @@ createNameSpace("realityEditor.avatar.iconMenu");
         parentDiv.appendChild(item);
 
         item.addEventListener('pointerup', (e) => {
-            callbacks.onAvatarIconClicked.forEach((cb) => {
+            callbacks.onMenuItemClicked.forEach((cb) => {
                 cb({
                     buttonText: textContent,
                     avatarObjectId: objectId,
@@ -191,15 +233,17 @@ createNameSpace("realityEditor.avatar.iconMenu");
             hideDropdown(parentDiv);
         });
     }
-
     /**
+     * Other modules can use this to detect when any avatar icon menu item was selected
      * @param {function} callback
      */
-    function registerAvatarIconClickEvent(callback) {
-        callbacks.onAvatarIconClicked.push(callback);
+    function onAvatarIconMenuItemSelected(callback) {
+        callbacks.onMenuItemClicked.push(callback);
     }
-
-    // create the container that all the avatar icon list elements will get added to
+    /**
+     * Create the container that all the avatar icon list elements will get added to
+     * @returns {HTMLDivElement}
+     */
     function createIconContainer() {
         let iconContainer = document.createElement('div');
         iconContainer.id = 'avatarIconContainer';
@@ -208,8 +252,16 @@ createNameSpace("realityEditor.avatar.iconMenu");
         document.body.appendChild(iconContainer)
         return iconContainer;
     }
-
-    // create an icon for this avatar, and add hover event listeners to show tooltip with full name
+    /**
+     * Create an icon for this avatar, and add hover event listeners to show tooltip with full name
+     * @param {HTMLElement} parent
+     * @param {string} objectKey
+     * @param {string|null} initials
+     * @param {number} index
+     * @param {boolean} isMyIcon
+     * @param {boolean} isEllipsis
+     * @returns {HTMLDivElement}
+     */
     function createAvatarIcon(parent, objectKey, initials, index, isMyIcon, isEllipsis) {
         let iconDiv = document.createElement('div');
         iconDiv.id = 'avatarIcon' + objectKey;
@@ -221,13 +273,9 @@ createNameSpace("realityEditor.avatar.iconMenu");
         iconImg.classList.add('avatarListIconImage');
         iconDiv.appendChild(iconImg);
 
-        if (isMyIcon) {
-            iconDiv.classList.add('avatarListIconMyAvatar');
-        }
-
+        // your icon has a different visual style (and different default image if no username/initials set)
         if (initials) {
             iconImg.src = 'svg/avatar-initials-background-dark.svg';
-
             let iconInitials = document.createElement('div');
             iconInitials.classList.add('avatarListIconInitials');
             iconInitials.innerText = initials;
@@ -240,6 +288,7 @@ createNameSpace("realityEditor.avatar.iconMenu");
             }
         }
 
+        // color the avatar icon to match the avatar color (the same color used by cursor, pointer, etc)
         let color = realityEditor.avatar.utils.getColor(realityEditor.getObject(objectKey));
         let lightColor = realityEditor.avatar.utils.getColorLighter(realityEditor.getObject(objectKey));
         if (isMyIcon && color) {
@@ -256,15 +305,23 @@ createNameSpace("realityEditor.avatar.iconMenu");
 
         return iconDiv;
     }
-
+    /**
+     * Helper function to detect if any menus are visible
+     * @returns {boolean}
+     */
     function areAnyDropdownsShown() {
         return Array.from(document.querySelectorAll('.avatarListIconDropdown')).some(dropdown => {
             return dropdown && dropdown.classList && !dropdown.classList.contains('hiddenDropdown');
         });
     }
-
-    // shows a tooltip that either says the name, or "You" or "Anonymous" if no name is provided, or a list of extra names
-    function showFullNameTooltip(element, name, isMyAvatar) {
+    /**
+     * shows a tooltip that either says the name, or "You" or "Anonymous", or a list of extra names
+     * @param {HTMLElement} element
+     * @param {string|null} name
+     * @param {boolean} isMyAvatar
+     * @param {boolean} isEllipsis
+     */
+    function showFullNameTooltip(element, name, isMyAvatar, isEllipsis) {
         // only show it if there aren't any dropdown menus shown
         if (areAnyDropdownsShown()) {
             return;
@@ -292,15 +349,17 @@ createNameSpace("realityEditor.avatar.iconMenu");
             container.appendChild(tooltipArrow);
         }
 
-        const clickActionText = 'click for options';
+        const clickActionText = isEllipsis ? '' : ' (click for options)';
         const nameText = isMyAvatar ? (name ? `${name} (You)` : 'You') : (name || 'Anonymous');
-        nameDiv.innerText = `${nameText} (${clickActionText})`;
+        nameDiv.innerText = `${nameText}${clickActionText}`;
         let clickActionTextWidth = 8 * clickActionText.length;
         let width = Math.max(120, ((nameDiv.innerText.length - clickActionText.length)) * 12 + clickActionTextWidth);
         nameDiv.style.width = width + 'px';
         container.style.display = '';
     }
-
+    /**
+     * Hides the tooltip, if shown
+     */
     function hideFullNameTooltip() {
         let nameDiv = document.getElementById('avatarListHoverName');
         if (nameDiv) {
@@ -310,7 +369,7 @@ createNameSpace("realityEditor.avatar.iconMenu");
 
     exports.initService = initService;
     exports.renderAvatarIconList = renderAvatarIconList;
-    exports.registerAvatarIconClickEvent = registerAvatarIconClickEvent;
+    exports.onAvatarIconMenuItemSelected = onAvatarIconMenuItemSelected;
     exports.MENU_ITEMS = MENU_ITEMS;
 
 }(realityEditor.avatar.iconMenu));
