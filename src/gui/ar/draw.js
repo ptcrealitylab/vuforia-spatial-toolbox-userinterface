084870f227a608bb843a9e36db6de03962cc4953
Rik de Leeuw <rdeleeuw@ptc.com>
2024-04-16 17:03:45 +0200

    fixed depth buffer issue on vision pro 1.1 by enabling anti aliasing removed early exit in renderloop

--- src/gui/ar/draw.js
+++ src/gui/ar/draw.js
@@ -277,7 +277,6 @@ realityEditor.gui.ar.draw.update = function (visibleObjects) {
         });
     }
 
-    if (!realityEditor.gui.ar.draw.frameNeedsToBeRendered) { return; } // don't recompute multiple times between a single animation frames
     realityEditor.gui.ar.draw.frameNeedsToBeRendered = false; // gets set back to true by requestAnimationFrame code
     
     var objectKey;
--- src/gui/scene/Renderer.js
+++ src/gui/scene/Renderer.js
@@ -122,7 +122,7 @@ class Renderer {
      * @param {HTMLCanvasElement} domElement 
      */
     constructor(domElement) {
-        this.#renderer = new THREE.WebGLRenderer({canvas: domElement, alpha: true, antialias: false});
+        this.#renderer = new THREE.WebGLRenderer({canvas: domElement, alpha: true, antialias: true});
         this.#renderer.setPixelRatio(window.devicePixelRatio);
         this.#renderer.setSize(window.innerWidth, window.innerHeight);
         this.#renderer.outputEncoding = THREE.sRGBEncoding;
