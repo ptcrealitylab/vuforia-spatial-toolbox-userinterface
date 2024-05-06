de1ca1fd12ca1bca0536a0a869f1a5dda3969ae3
James Hobin <jhobin@ptc.com>
2024-05-06 14:41:29 -0400

    Integrate MPMLink
    - Refactor out import steps button
    - Replace xml file upload with windchill integration; Visualize process plan data in region card
    - Use regionCard as step label
    - Select process plan based on name
    - Use more of MPMLink API; Add motionStudy deep links
      Refactor out low-level MPMLink API to MPMAPI. Support linking steps with
      fragment in url. Upload some region card data to MPMLink

---
+++ src/motionStudy/ImportStepsButton.js
@@ -0,0 +1,41 @@
+export class ImportStepsButton {
+    constructor(motionStudy, windchill) {
+        this.motionStudy = motionStudy;
+        this.windchill = windchill;
+
+        this.onImportClick = this.onImportClick.bind(this);
+
+        this.container = document.createElement('div');
+        this.container.id = 'analytics-step-file-upload-container';
+        this.container.classList.add('analytics-button-container');
+
+        this.stepFileInputLabel = document.createElement('label');
+        // this.stepFileInputLabel.classList.add('analytics-step');
+        this.stepFileInputLabel.setAttribute('for', 'analytics-step-file');
+        this.stepFileInputLabel.textContent = 'Import Step File';
+
+        this.stepFileInput = document.createElement('input');
+        this.stepFileInput.id = 'analytics-step-file';
+        this.stepFileInput.type = 'button';
+        this.stepFileInput.addEventListener('click', this.onImportClick);
+
+        this.container.appendChild(this.stepFileInputLabel);
+        this.container.appendChild(this.stepFileInput);
+    }
+
+    show() {
+        this.container.style.display = '';
+    }
+
+    hide() {
+        this.container.style.display = 'none';
+    }
+
+    async onImportClick() {
+        let plans = await this.windchill.getProcessPlans('VR_DEMO_PP');
+        console.log('found process plans', plans);
+        let plan = plans[0];
+        this.motionStudy.setProcessPlan(plan);
+        this.motionStudy.titleInput.textContent = plan.name;
+    }
+}
