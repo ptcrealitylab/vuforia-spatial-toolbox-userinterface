c599d47125b2734a7f8627ba28ebef32679ab400
James Hobin <jhobin@ptc.com>
2024-06-10 10:55:11 -0400

    Add get/setTitle

--- src/motionStudy/motionStudy.js
+++ src/motionStudy/motionStudy.js
@@ -367,6 +367,14 @@ export class MotionStudy {
         this.pinnedRegionCardsContainer.appendChild(this.titleInput);
     }
 
+    getTitle() {
+        return this.titleInput.textContent;
+    }
+
+    setTitle(title) {
+        this.titleInput.textContent = title;
+    }
+
     draw() {
         if (this.container.parentElement) {
             this.timeline.draw();
@@ -677,7 +685,7 @@ export class MotionStudy {
         });
 
         if (data.title) {
-            this.titleInput.textContent = data.title;
+            this.setTitle(data.title);
         }
 
         for (let pinnedRegionCard of this.pinnedRegionCards) {
@@ -906,8 +914,8 @@ export class MotionStudy {
                     valueAddWasteTime: this.valueAddWasteTimeManager.toJSON()
                 },
             );
-            if (this.titleInput.textContent) {
-                motionStudyData.title = this.titleInput.textContent;
+            if (this.getTitle()) {
+                motionStudyData.title = this.getTitle();
             }
             realityEditor.network.realtime.writePublicData(objectKey, frameKey, frameKey + 'storage', 'analyticsData', motionStudyData);
 
