d6602f2996a0d02edb579db21e77c2aefd305565
James Hobin <jhobin@ptc.com>
2024-10-14 14:50:36 -0400

    Truncate numbers in csv export

--- src/motionStudy/motionStudy.js
+++ src/motionStudy/motionStudy.js
@@ -1356,6 +1356,13 @@ export class MotionStudy {
                 values.push(...selectedValuesForKey);
             });
 
+            values = values.map(v => {
+                if (typeof v === 'number') {
+                    return v.toFixed(3);
+                }
+                return v;
+            });
+
             lines.push(values);
         }
 
